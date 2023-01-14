7b976900586c1ac32bb736b0e26fa4af2364eea9
Anastasiya <anastasiya.sachuk@mail.ru>
2023-01-15 00:42:47 +0300

    Add Examples 005 Condition if else

--- Examples005_ConditionIfElse/Program.cs
+++ Examples005_ConditionIfElse/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine()!;
+
+if(username.ToLower() == "ася")
+{
+  Console.WriteLine("Ура, это же АСЯ!");  
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
